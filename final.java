f77dcb8deece17fc48cde0a9fb6e5303086d03de
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2022-12-04 11:47:30 +0530

    Create final.java

---
+++ final.java
@@ -0,0 +1,17 @@
+public class Final
+{
+  public static void main(String[] args)
+{
+   final String Name = "Digvijay Singh";
+   System.out.println("Name" + "=" + Name);
+   final int Roll = 01;
+   System.out.println("Roll" + "=" + Roll);
+   final int STUDENT_MARKS = 100;
+   System.out.println("STUDENT_MARKS" + "=" + STUDENT_MARKS);
+   final String Teacher;
+{
+   String Teacher1 ="Java Sir";
+   System.out.println("Teacher" + "=" + Teacher1);
+  }
+ }
+}
